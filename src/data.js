792eaa9839e5732a9b12163925d690d0ca348b24
davidballezaa <davidballezaa@gmail.com>
2022-02-05 23:16:08 -0600

    add data to fetch application

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export default [
+    {
+        id: 1,
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://images.unsplash.com/photo-1589308078059-be1415eab4c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+    {
+        id: 2,
+        title: "Wadi Rum Village",
+        location: "Jordan",
+        googleMapsUrl: "https://goo.gl/maps/kF4eQVbkh3z7kALi8",
+        startDate: "23 Mar, 2022",
+        endDate: "27 Mar, 2022",
+        description: "Wadi Rum is a spectacularly scenic desert valley (wadi in Arabic) in southern Jordan. It is also known as \"Valley of the Moon\".",
+        imageUrl: "https://images.unsplash.com/photo-1554357475-accb8a88a330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+    },
+    {
+        id: 3,
+        title: "Mt. Kilimanjaro",
+        location: "Tanzania",
+        googleMapsUrl: "https://goo.gl/maps/YLi7KkrQm8Qyc2798",
+        startDate: "4 Apr, 2022",
+        endDate: "8 Apr, 2022",
+        description: "With an unprecedented elevation gain, climbing Kilimanjaro is a spectacle of diversity as one traverses its many ecosystems and climactic changes.",
+        imageUrl: "https://images.unsplash.com/photo-1589177900326-900782f88a55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
+    },
+    {
+        id: 4,
+        title: "Djemaa el fna",
+        location: "Morocco",
+        googleMapsUrl: "https://goo.gl/maps/EYNyevuei7Wnxv9T6",
+        startDate: "11 May, 2022",
+        endDate: "13 May, 2022",
+        description: "The world's most exciting town square, Djemaa el Fna reminds you you're in Africa. In the heart of the old city of Marrakech.",
+        imageUrl: "https://images.unsplash.com/photo-1549140600-78c9b8275e9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    },
+    {
+        id: 5,
+        title: "Sphinx",
+        location: "Egypt",
+        googleMapsUrl: "https://goo.gl/maps/WEj8L9X3fKRNMGaB9",
+        startDate: "17 May, 2022",
+        endDate: "23 May, 2022",
+        description: "This colossal temple is on the way down to Egypt's Valley Temple of King Chefren. The body of a lion with a human head is 70 meters long and 20 meters high.",
+        imageUrl: "https://images.unsplash.com/photo-1562679299-266edbefd6d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
+    },
+    {
+        id: 6,
+        title: "Victoria Falls",
+        location: "Zambia",
+        googleMapsUrl: "https://g.page/vicfalls?share",
+        startDate: "18 Nov, 2022",
+        endDate: "23 Nov, 2022",
+        description: "Straddling the border between Zambia and Zimbabwe, Victoria Falls is roughly twice as deep and wide as Niagara Falls.",
+        imageUrl: "https://images.unsplash.com/photo-1603201236596-eb1a63eb0ede?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    }
+]
