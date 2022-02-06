aa660d7a408696e14165f0e8591b6506f6987642
davidballezaa <davidballezaa@gmail.com>
2022-02-05 23:17:25 -0600

    add cards to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import './App.scss';
 import Navbar from './components/Navbar'
+import Card from './components/Card'
+import data from "./data"
 
 function App() {
+  const cards = data.map(item => {
+    return <Card key={item.id} {...item} />
+  })
   return (
     <div className="App">
       <Navbar />
+      {cards}
     </div>
   );
 }
