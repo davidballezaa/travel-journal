61f64fbbf56d804e67d43b7c037705b8593b7d6c
davidballezaa <davidballezaa@gmail.com>
2022-02-06 00:16:12 -0600

    add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import "../styles/Footer.scss"
+
+export default function Footer() {
+  return (
+    <footer>
+      <a href="https://github.com/davidballezaa">By David Balleza</a>
+    </footer>
+  )
+}
